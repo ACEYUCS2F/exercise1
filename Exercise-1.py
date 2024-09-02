37b198544a0d90ce0191e411d3a191b90c3ac613
ACEYUCS2F <144134954+ACEYUCS2F@users.noreply.github.com>
2024-09-02 22:54:59 +0800

    Add files via upload

---
+++ Exercise-1.py
@@ -0,0 +1,33 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+# My first simple script
+print("Hello, I'm Ace Yu! BSCS 3F")
+
+number = 123
+name = "Ace"
+
+# Print variable values
+print("Number:", number)
+print("Name:", name)
+
+# Group members' names (excluding my own)
+member1 = "Angela"
+member2 = "Angel"
+member3 = "Hersley"
+member4 = "Elopre"
+
+# Assign values to multiple variables in one line
+x, y, z = 1, 2, 3
+print("Values of x, y, z:", x, y, z)
+
+# Assign the same value to multiple variables in one line
+a = b = c = 10
+print("Values of a, b, c:", a, b, c)
+
+# Print group members
+print("Group members:")
+print("Member 1:", member1)
+print("Member 2:", member2)
+print("Member 3:", member3)
+print("Member 4:", member4)
